b6047b30b9f61b65ef6508df0d2fec3b8b28c58c
Axel David Plascencia Silva <pablitobanana1@gmail.com>
2022-05-05 00:39:01 -0500

    catas personalizadas, con informacion unica

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="cards" class=" ">
-    <div class="container-xxl">
+    <div class="container-xxl mb-3">
       <h1>Selecciona Pokemos</h1>
-      <div class="row" v-for="(n,o) in 5" :key="o">
-        <div class="col-md-4" v-for="(n,i) in 3" :key="i">
+      <div class="row row-cols-1 row-cols-md-3 g-3">
+        <div class="col-md-4" v-for="(pokemon,i) in pokemons" :key="i">
           <div class="card mb-3 mt-3 m-sm-auto ms-md-auto" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-12 col-lg-4">
-      <img :src="picture" class="img-fluid rounded-start" alt="...">
+      <img :src="pokemon.sprites.other.dream_world.front_default" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-12 col-lg-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h3 class="card-title">{{pokemon.name}}</h3>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <p class="card-text"><small class="text-muted">{{pokemon.types[0].type.name}}</small></p>
       </div>
     </div>
   </div>
@@ -25,10 +25,17 @@
 </template>
 
 <script>
+import { ref,onMounted } from "vue"
 export default{
   setup(){
-    const picture = "https://www.lavanguardia.com/files/content_image_mobile_filter/uploads/2016/07/26/5fa2d21ddebf4.jpeg"
-    return{picture}
+    const pokemons = ref([]);
+    onMounted( async () =>{
+      for (let i = 1; i <= 65; i++) {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/"+i)
+        pokemons.value[i-1] = await res.json()
+      }
+    })
+    return{pokemons}
   } 
 };
 </script>
