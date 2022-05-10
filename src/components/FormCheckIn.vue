11a18dc61cf8a9a0b2cdf2d470de1ce5bcff2307
Axel David Plascencia Silva <pablitobanana1@gmail.com>
2022-05-09 23:57:52 -0500

    Cambiando fuente en el titulo de formulario Checkin

--- src/components/FormCheckIn.vue
+++ src/components/FormCheckIn.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="form-check-in" class="container ">
     <form class="row g-3 border mt-5 col-md-8 col-lg-5 m-auto p-4">
-      <h1>Check in</h1>
+      <h2>Check in</h2>
   <div class="col-md-12">
     <input type="text" class="form-control" id="inputnombreUsuario" placeholder="Nombre de Usuario">
   </div>
